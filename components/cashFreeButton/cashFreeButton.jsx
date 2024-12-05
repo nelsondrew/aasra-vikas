f065c3e0b5822184cddf8de69c8538cc5f659ca1
Nelson drew <nelsondrew07@gmail.com>
2024-12-05 22:11:40 +0530

    cash free button update

--- components/cashFreeButton/cashFreeButton.jsx
+++ components/cashFreeButton/cashFreeButton.jsx
@@ -1,22 +1,34 @@
 import { load } from "@cashfreepayments/cashfree-js";
 import { createCashfreeOrder } from "../../api/cashfreeapi";
+import { PAYMENT_STRING, PROCESSING_FEE, PRODUCTION, REDIRECT_URL, TEST_EMAIL } from "../../constants/constants";
+import { useEffect, useState } from "react";
 
-function Checkout() {
-  let cashfree;
-  var initializeSDK = async function () {
-    cashfree = await load({
-      mode: "production",
+
+function CashFreeButton({
+  mobileNumber,
+  customerId
+}) {
+  const [cashfree ,setCashfree] = useState(null);
+  const initializeSDK = async function () {
+    let cashFreeObj = await load({
+      mode: PRODUCTION,
     });
+    setCashfree(cashFreeObj)
   };
-  initializeSDK();
+
+  useEffect(() => {
+    initializeSDK();
+  },[]);
+
+
 
   const doPayment = async () => {
     const { paymentSessionId } = await createCashfreeOrder({
-      amount: 1,
-      customerEmail: "test@test.com",
-      customerPhone: "9999999999",
-      customerId: "abcdsees",
-      returnUrl: "https://www.aasravikas.com/register",
+      amount: PROCESSING_FEE,
+      customerEmail: TEST_EMAIL,
+      customerPhone: mobileNumber,
+      customerId: customerId,
+      returnUrl: REDIRECT_URL,
     });
     let checkoutOptions = {
       paymentSessionId: paymentSessionId,
@@ -24,10 +36,6 @@ function Checkout() {
     };
     cashfree.checkout(checkoutOptions).then((result) => {
       if (result.error) {
-        // This will be true whenever user clicks on close icon inside the modal or any error happens during the payment
-        console.log(
-          "User has closed the popup or there is some payment error, Check for Payment Status"
-        );
         console.log(result.error);
       }
       if (result.redirect) {
@@ -37,11 +45,6 @@ function Checkout() {
         console.log("Payment will be redirected");
       }
       if (result.paymentDetails) {
-        // This will be called whenever the payment is completed irrespective of transaction status
-        console.log("Payment has been completed, Check for Payment Status");
-        console.log({
-            result
-        })
         console.log(result.paymentDetails.paymentMessage);
       }
     });
@@ -54,9 +57,9 @@ function Checkout() {
         id="renderBtn"
         onClick={doPayment}
       >
-        Pay ₹199
+        {PAYMENT_STRING}
       </button>
     </div>
   );
 }
-export default Checkout;
+export default CashFreeButton;
---
+++ constants/constants.js
@@ -0,0 +1,5 @@
+export const PROCESSING_FEE = 1;
+export const PRODUCTION = 'production'
+export const REDIRECT_URL = "https://www.aasravikas.com/register";
+export const TEST_EMAIL =  "test@test.com";
+export const PAYMENT_STRING = 'Pay ₹199'
