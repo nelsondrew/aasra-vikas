ab64642d851b2949d211be818e1ae601f4419a1c
Nelson drew <nelsondrew07@gmail.com>
2024-12-05 22:16:24 +0530

    cash free button cleanup

--- components/cashFreeButton/cashFreeButton.jsx
+++ components/cashFreeButton/cashFreeButton.jsx
@@ -1,28 +1,52 @@
 import { load } from "@cashfreepayments/cashfree-js";
 import { createCashfreeOrder } from "../../api/cashfreeapi";
-import { PAYMENT_STRING, PROCESSING_FEE, PRODUCTION, REDIRECT_URL, TEST_EMAIL } from "../../constants/constants";
+import {
+  PAYMENT_STRING,
+  PROCESSING_FEE,
+  PRODUCTION,
+  REDIRECT_URL,
+  TEST_EMAIL,
+} from "../../constants/constants";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
+const StyledCashFreeButtonContainer = styled.div`
+  .loader {
+    border: 4px solid #f3f3f3;
+    border-top: 4px solid #3498db;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    animation: spin 2s linear infinite;
+  }
 
-function CashFreeButton({
-  mobileNumber,
-  customerId
-}) {
-  const [cashfree ,setCashfree] = useState(null);
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+function CashFreeButton({ mobileNumber, customerId, onSuccess }) {
+  const [cashfree, setCashfree] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const initializeSDK = async function () {
     let cashFreeObj = await load({
       mode: PRODUCTION,
     });
-    setCashfree(cashFreeObj)
+    setCashfree(cashFreeObj);
   };
 
   useEffect(() => {
     initializeSDK();
-  },[]);
-
-
+  }, []);
 
   const doPayment = async () => {
+    setLoading(true);
     const { paymentSessionId } = await createCashfreeOrder({
       amount: PROCESSING_FEE,
       customerEmail: TEST_EMAIL,
@@ -36,30 +60,31 @@ function CashFreeButton({
     };
     cashfree.checkout(checkoutOptions).then((result) => {
       if (result.error) {
-        console.log(result.error);
+        setError(result.error);
+        setLoading(false);
       }
       if (result.redirect) {
         // This will be true when the payment redirection page couldnt be opened in the same window
         // This is an exceptional case only when the page is opened inside an inAppBrowser
         // In this case the customer will be redirected to return url once payment is completed
         console.log("Payment will be redirected");
+        setLoading(false);
       }
       if (result.paymentDetails) {
-        console.log(result.paymentDetails.paymentMessage);
+        if (typeof result?.paymentDetails?.paymentMessage === "string") {
+          setLoading(false);
+          onSuccess();
+        }
       }
     });
   };
 
   return (
-    <div>
-      <button
-        className="payment-button"
-        id="renderBtn"
-        onClick={doPayment}
-      >
-        {PAYMENT_STRING}
+    <StyledCashFreeButtonContainer>
+      <button className="payment-button" id="renderBtn" onClick={doPayment}>
+        {loading ? <div className="loader" /> : PAYMENT_STRING}
       </button>
-    </div>
+    </StyledCashFreeButtonContainer>
   );
 }
 export default CashFreeButton;
